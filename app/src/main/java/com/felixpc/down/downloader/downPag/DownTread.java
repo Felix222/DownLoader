df47191dd5853c963c31597a95d162169c5ad9fd
Felix222 <wt9402@163.com>
2016-04-11 23:50:49 +0800

    修改下载类等线程

---
+++ app/src/main/java/com/felixpc/down/downloader/downPag/DownTread.java
@@ -0,0 +1,7 @@
+package com.felixpc.down.downloader.downPag;
+
+/**
+ * Created by Felix on 2016/4/11.
+ */
+public class DownTread {
+}
--- app/src/main/java/com/felixpc/down/downloader/tool/Utiles.java
+++ app/src/main/java/com/felixpc/down/downloader/tool/Utiles.java
@@ -46,4 +46,5 @@ public class Utiles {
         }
         return sdDir.toString();
     }
+
 }

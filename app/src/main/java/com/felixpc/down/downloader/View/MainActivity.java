3be2414e19413121e9e5db9b68219780dcc44b2b
Felix222 <wt9402@163.com>
2016-04-13 23:28:11 +0800

    完成弹出输入框制作。

--- app/src/main/java/com/felixpc/down/downloader/View/MainActivity.java
+++ app/src/main/java/com/felixpc/down/downloader/View/MainActivity.java
@@ -2,11 +2,16 @@ package com.felixpc.down.downloader.View;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.felixpc.down.downloader.R;
+import com.felixpc.down.downloader.widget.InputDialog;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+    private InputDialog inputDialog;
+    private StringBuffer inputFileUrl;
+
 
     @Override
     public void initContentView(Bundle savedInstanceState) {
@@ -16,7 +21,38 @@ public class MainActivity extends BaseActivity {
     @Override
     public void initView() {
         Log.i(TAG, "初始化控件");
-        ((TextView) findViewById(R.id.testlay)).setText("测试空指针");
+        ((TextView) findViewById(R.id.titlebar_title)).setText("任务列表");
+        findViewById(R.id.titlebar_add).setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.titlebar_add:
+                if (inputDialog == null) {
+                    inputDialog = new InputDialog(this);
+                    inputDialog.setCanceledOnTouchOutside(true);
+                    inputDialog.addOKButton(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Log.i(TAG, "ok按钮");
+                            inputFileUrl = new StringBuffer(inputDialog.getInputUrl());
+                            inputDialog.dismiss();
+                            inputDialog = null;
+                        }
+                    }).addCancleButton(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            inputDialog.dismiss();
+                            inputDialog = null;
+
+                        }
+                    });
+                }
+                if (!inputDialog.isShowing()) {
+                    inputDialog.show();
+                }
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/com/felixpc/down/downloader/widget/InputDialog.java
@@ -0,0 +1,100 @@
+package com.felixpc.down.downloader.widget;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.util.Log;
+import android.view.Display;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.MaterialEditText;
+import android.widget.TextView;
+
+import com.felixpc.down.downloader.R;
+
+/**
+ * Created by Felix on 2016/4/13.
+ */
+public class InputDialog extends Dialog {
+    private String TAG = InputDialog.class.getName() + " ";
+    private MaterialEditText urleditText;
+    private Button okButton, cancleButton;
+    private LinearLayout input_parent_lay;
+    private WindowManager windowManager;
+
+    public InputDialog(Context context) {
+        super(context, R.style.MyDialogStyle);
+
+        initDialog();
+    }
+
+
+    private void initDialog() {
+        View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.input_file_url, null);
+        urleditText = (MaterialEditText) dialogView.findViewById(R.id.path_lay);
+
+        okButton = (Button) dialogView.findViewById(R.id.inputPathLay_ok);
+        cancleButton = (Button) dialogView.findViewById(R.id.inputPathLay_cancle);
+
+        input_parent_lay = (LinearLayout) dialogView.findViewById(R.id.input_parent_lay);
+
+        urleditText = (MaterialEditText) dialogView.findViewById(R.id.path_lay);
+        urleditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    Log.i(TAG, " 键盘回车事件");
+                } else if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    Log.i(TAG, " 键盘关闭事件");
+                    try {
+                        input_parent_lay.requestFocus();
+                        urleditText.clearFocus();
+                    } catch (Exception e) {
+                    }
+                }
+                return false;
+            }
+        });
+        super.setContentView(dialogView);
+
+    }
+
+
+    public InputDialog addOKButton(View.OnClickListener listener) {
+        if (this.okButton != null) {
+            this.okButton.setOnClickListener(listener);
+        }
+        return this;
+    }
+
+    public String getInputUrl() {
+        return urleditText.getText().toString().trim();
+    }
+
+    public InputDialog addCancleButton(View.OnClickListener listener) {
+        if (this.cancleButton != null) {
+            this.cancleButton.setOnClickListener(listener);
+        }
+        return this;
+    }
+
+    @Override
+    public void dismiss() {
+        input_parent_lay.requestFocus();
+        super.dismiss();
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        WindowManager.LayoutParams lp = this.getWindow().getAttributes();
+        lp.width = (int) (display.getWidth() * 0.75); //设置宽度
+        this.getWindow().setAttributes(lp);
+    }
+}
